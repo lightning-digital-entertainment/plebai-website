0f771944c1e2d9ca9301a3c392307bf7a308ab99
starbackr-dev <arun@starbackr.com>
2023-07-03 09:22:46 -0400

    initial commit

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  // Configure Content Wind here
+  cover: '/cover.jpg',
+  socials: {
+    twitter: 'getcurrent_io',
+    github: 'lightning-digital-entertainment/plebai'
+  },
+  prose: {
+    h1: {
+      icon: 'carbon:sailboat-coastal'
+    }
+  }
+})
