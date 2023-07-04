5a6d622659469f82a60b9bb35aaadfd9b1331bcf
starbackr-dev <arun@starbackr.com>
2023-07-04 12:23:55 -0400

    changed cover image

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   // Configure Content Wind here
-  cover: '/cover.jpg',
+  cover: '/cover.png',
   socials: {
     twitter: 'getcurrent_io',
     github: 'lightning-digital-entertainment/plebai'
