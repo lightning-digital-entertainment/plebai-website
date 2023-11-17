27497562515bdebeb59595e768336ace22a36bfb
starbackr-dev <arun@starbackr.com>
2023-11-16 23:01:21 -0500

    added chatbox and sdk

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,30 @@
 export default defineNuxtConfig({
-  extends: 'content-wind'
-})
+  extends: 'content-wind',
+
+  app: {
+    head: {
+      script: [
+        {
+          // injects the config based on the environment
+            src: "https://i.current.fyi/static/plebai.js",      
+        },
+        {
+          src: 'https://i.current.fyi/static/constants.js'
+
+        },
+      ],
+      link: [
+
+          {
+            rel:"stylesheet",
+            href:"https://i.current.fyi/static/styles.css"
+
+          }
+
+      ]
+    },
+  }
+}
+
+
+)
