5cc275192cd1ffddfcb64188034d8a4b4d4b6544
starbackr-dev <arun@starbackr.com>
2023-11-17 14:23:37 -0500

    updated chatbox

---
+++ constants.js
@@ -0,0 +1,8 @@
+var plebAiConf = {
+    chatTitle: "Chat with Orange Pill GPT",   
+    agentKey:"e301925b25ea966e27711d1fc7de65183752dfc2f5830505964d485228f8182d",
+    primaryColor: "#009687",
+    secondaryColor: "#70e5d9",
+    buttonColor: "#009687"
+
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,10 @@ export default defineNuxtConfig({
       script: [
         {
           // injects the config based on the environment
-            src: "https://i.current.fyi/static/plebai.js",      
+            src: "https:/i.current.fyi/static/plebai.js",      
         },
         {
-          src: 'https://i.current.fyi/static/constants.js'
+          src: 'https://starbackr-images.s3.amazonaws.com/static/constants.js'
 
         },
       ],
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
 
           {
             rel:"stylesheet",
-            href:"https://i.current.fyi/static/styles.css"
+            href:"https:/i.current.fyi/static/styles.css"
 
           }
 
